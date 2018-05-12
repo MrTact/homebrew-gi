70a927cbec5a1be7d4ce79afb0b3c8592a00feae
Tim Keating <mrtact@gmail.com>
2018-05-11 19:48:13 -0500

    Update formula for 0.4.0 release

--- gi.rb
+++ gi.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 class Gi < Formula
-  VERSION = "0.3.0"
+  VERSION = "0.4.0"
 
   desc "Alternate command line client for git"
   homepage "https://gitlab.com/mrtact/gi"
   url "https://s3-us-west-2.amazonaws.com/org.tkeating.gi/gi-#{VERSION}.tar.gz"
-  sha256 "fd420bc88f81057660672095479d9fa55bf195c62860724b69ba92d5bf343ab2"
+  sha256 "416c0702178d87156a70b238835159f3d7f76cb911cd6131490b56c5f8f4c2f8"
 
   bottle :unneeded
 
