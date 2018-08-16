8a7a591fdded13a052fe7c79328de2729de1a861
Tim Keating <mrtact@gmail.com>
2018-08-16 00:24:13 -0500

    0.5.0 for testing purposes

--- gi.rb
+++ gi.rb
@@ -1,19 +1,19 @@
 require "formula"
 
 class Gi < Formula
-  VERSION = "0.4.0"
+  VERSION = "0.5.0"
 
   desc "Alternate command line client for git"
   homepage "https://gitlab.com/mrtact/gi"
   url "https://s3-us-west-2.amazonaws.com/org.tkeating.gi/gi-#{VERSION}.tar.gz"
-  sha256 "8d1c8a7a511b4ba4d972a67780b3d6acaa785a229c7a3ecd508dc329d390796a"
+  sha256 "87f0b902c0b2fc94fd80db857cb212b8e85ef4fdfe220145d39ef4735e030b1e"
 
   bottle :unneeded
 
   def install
-    inreplace "bin/gi", "GI_LIBEXEC", "#{libexec}" 
+    inreplace "bin/gi", "GI_LIBEXEC", "#{libexec}"
   	libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/gi" => "gi"
-  	# man1.install libexec/"share/man/man1/srcclr.1"
+  	# man1.install libexec/"share/man/man1/gi.1"
   end
 end
