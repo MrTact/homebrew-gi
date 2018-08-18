e4fd1b0b47622eb0177e72db618fbd38f5621785
Tim Keating <mrtact@gmail.com>
2018-08-18 18:26:47 -0500

    Add man file support

--- gi.rb
+++ gi.rb
@@ -6,7 +6,7 @@ class Gi < Formula
   desc "Alternate command line client for git"
   homepage "https://gitlab.com/mrtact/gi"
   url "https://s3-us-west-2.amazonaws.com/org.tkeating.gi/gi-#{VERSION}.tar.gz"
-  sha256 "96cfd6c465505e9e0004eee969bbff59045c8d6d027a3924e7fe50167788d41c"
+  sha256 "815f3bcb567b2d0a7aa17e91190e424be902a2cf0415f8c847a87275a654afe0"
 
   bottle :unneeded
 
@@ -14,6 +14,6 @@ class Gi < Formula
     inreplace "bin/gi", "GI_LIBEXEC", "#{libexec}"
   	libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/gi" => "gi"
-  	# man1.install libexec/"share/man/man1/gi.1"
+  	man1.install "#{libexec}/share/man/man1/gi.1"
   end
 end
