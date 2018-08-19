69b065fb0b70328bea3221c1a347d76e552d7838
Tim Keating <mrtact@gmail.com>
2018-08-19 10:24:26 -0500

    Update SHA

--- gi.rb
+++ gi.rb
@@ -6,7 +6,7 @@ class Gi < Formula
   desc "Alternate command line client for git"
   homepage "https://gitlab.com/mrtact/gi"
   url "https://s3-us-west-2.amazonaws.com/org.tkeating.gi/gi-#{VERSION}.tar.gz"
-  sha256 "d60931f4d3caf61db72d94dfd18d63a01ecd95e9227920ae386dcc81cbc529bb"
+  sha256 "865cfba626966e036e652d8dfd1415359383f1d5fee92e7636fd154aaed03760"
 
   bottle :unneeded
 
