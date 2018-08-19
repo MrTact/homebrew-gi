4c7c7aac2d03e7a2578bd219dca3aa5564603e79
Tim Keating <mrtact@gmail.com>
2018-08-19 13:30:41 -0500

    Update SHA & man page install

--- gi.rb
+++ gi.rb
@@ -6,7 +6,7 @@ class Gi < Formula
   desc "Alternate command line client for git"
   homepage "https://gitlab.com/mrtact/gi"
   url "https://s3-us-west-2.amazonaws.com/org.tkeating.gi/gi-#{VERSION}.tar.gz"
-  sha256 "865cfba626966e036e652d8dfd1415359383f1d5fee92e7636fd154aaed03760"
+  sha256 "95366cb0acdfe0b89ab4482369387d947d3ec8f489bf3f6c2e80662c2c7927c4"
 
   bottle :unneeded
 
@@ -14,6 +14,6 @@ class Gi < Formula
     inreplace "bin/gi", "GI_LIBEXEC", "#{libexec}"
   	libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/gi" => "gi"
-  	man1.install "gi.1"
+  	man1.install_symlink "#{libexec}/doc/gi.1"
   end
 end
