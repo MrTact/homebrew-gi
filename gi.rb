e7232357b8a8f7842d757e1f70ff24f6174068a8
Tim Keating <mrtact@gmail.com>
2019-01-25 00:13:35 -0600

    0.5.1 fix an åerror

--- gi.rb
+++ gi.rb
@@ -6,7 +6,7 @@ class Gi < Formula
   desc "Alternate command line client for git"
   homepage "https://gitlab.com/mrtact/gi"
   url "https://s3-us-west-2.amazonaws.com/org.tkeating.gi/gi-#{VERSION}.tar.gz"
-  sha256 "bb3f2d9c5b03a00a6feb49062739e213b45cb50b8442682b8e21291ee6fb5ff4"
+  sha256 "cd5aace6c0dfd87f26cdd718d33b3efb61866cc3fa2f40e65775c97140157479"
 
   bottle :unneeded
 
@@ -15,6 +15,6 @@ class Gi < Formula
   	libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/gi" => "gi"
   	man1.install_symlink "#{libexec}/doc/gi.1"
-  	stdout.puts "Bash completion script installed to #{libexec}/gi-complete.bash. Source this file to enable completion."
+  	ohai "Bash completion script installed to #{libexec}/gi-complete.bash. Source this file to enable completion."
   end
 end
