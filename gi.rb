9ecab86c09c4476cc8a780116c96553ebfb2c44f
Tim Keating <mrtact@gmail.com>
2019-01-25 00:00:00 -0600

    Release 0.5.1

--- gi.rb
+++ gi.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 class Gi < Formula
-  VERSION = "0.5.0"
+  VERSION = "0.5.1"
 
   desc "Alternate command line client for git"
   homepage "https://gitlab.com/mrtact/gi"
   url "https://s3-us-west-2.amazonaws.com/org.tkeating.gi/gi-#{VERSION}.tar.gz"
-  sha256 "95366cb0acdfe0b89ab4482369387d947d3ec8f489bf3f6c2e80662c2c7927c4"
+  sha256 "bb3f2d9c5b03a00a6feb49062739e213b45cb50b8442682b8e21291ee6fb5ff4"
 
   bottle :unneeded
 
@@ -15,5 +15,6 @@ class Gi < Formula
   	libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/gi" => "gi"
   	man1.install_symlink "#{libexec}/doc/gi.1"
+  	stdout.puts "Bash completion script installed to #{libexec}/gi-complete.bash. Source this file to enable completion."
   end
 end
